e82392fc0daf5d2f9f9eb9011877c8b3ab449fc4
Vũ Duy <ducduy23899@gmail.com>
2022-06-11 17:48:28 +0700

    theme

--- src/view/ExcelView.js
+++ src/view/ExcelView.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Readexcel from '../components/Readexcel';
 import Button from '@material-ui/core/Button';
 
+
 export default function ExcelView() {
     return (
-        <div>
+        <div className='container'>
             <a href='/topsis-demo.xlsx' download>
                 <Button variant="contained" >Download</Button>
             </a>
